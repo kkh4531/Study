34ef956ad63d5d3d75a16a0a23a3ffac68792ad7
kimkihyeon <rlgus4531@naver.com>
2025-02-13 19:04:41 +0900

    인프런 자바 알고리즘 인강 1-03

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,49 +5,60 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-2. 대소문자 변환
+3. 문장 속 단어
 설명
 
-대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요.
+
+문장속의 각 단어는 공백으로 구분됩니다.
 
 
 입력
 
-첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
-
-문자열은 영어 알파벳으로만 구성되어 있습니다.
+첫 줄에 길이가 100을 넘지 않는 한 개의 문장이 주어집니다. 문장은 영어 알파벳으로만 구성되어 있습니다.
 
 
 출력
 
-첫 줄에 대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 출력합니다.
+첫 줄에 가장 긴 단어를 출력한다. 가장 길이가 긴 단어가 여러개일 경우 문장속에서 가장 앞쪽에 위치한
+
+단어를 답으로 합니다.
 
 
 예시 입력 1
 
-StuDY
+it is time to study
 예시 출력 1
 
-sTUdy
+study
  */
 public class Main {
 
-    public static StringBuilder solution(String str) {
-        StringBuilder res = new StringBuilder();
-        for (char c : str.toCharArray()) {
-            if (Character.isLowerCase(c)) {
-                res.append(Character.toUpperCase(c));
-            } else {
-                res.append(Character.toLowerCase(c));
+    public static String solution(String str[]) {
+        int max = Integer.MIN_VALUE;
+        String res = "";
+        for (String s : str) {
+            if (s.length() > max) {
+                max = s.length();
             }
         }
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].length() == max) {
+                list.add(str[i]);
+            }
+        }
+
+        res = list.get(0);
         return res;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = bufferedReader.readLine();
+        String str[] = bufferedReader.readLine().split(" ");
+
         System.out.println(solution(str));
     }
 
