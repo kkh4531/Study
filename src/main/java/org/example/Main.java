8a36ec7e045d5a6aa81ff623cf84d17aaf24e5b0
kimkihyeon <rlgus4531@naver.com>
2025-02-13 16:43:21 +0900

    인프런 자바 알고리즘 인강 1-01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,47 +4,44 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/*
+1. 문자 찾기
+설명
+
+한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+
+대소문자를 구분하지 않습니다.문자열의 길이는 100을 넘지 않습니다.
+
+입력
+
+첫 줄에 문자열이 주어지고, 두 번째 줄에 문자가 주어진다.
+
+문자열은 영어 알파벳으로만 구성되어 있습니다.
+
+
+출력
+
+첫 줄에 해당 문자의 개수를 출력한다
+ */
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder();
 
-        int N = Integer.parseInt(bufferedReader.readLine()); // 자료구조 개수
-
-        int A[] = new int[N]; // 자료구조 형태 (queue or stack)
-        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken()); //
-        }
-
-        int B[] = new int[N]; // 각 자료구조에 들어갈 첫 원소
-        st = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < N; i++) {
-            B[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int M = Integer.parseInt(bufferedReader.readLine()); // 삽입할 값 길이
-
-        int C[] = new int[M]; // 삽입할 값들
-        st = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < M; i++) {
-            C[i] = Integer.parseInt(st.nextToken());
-        }
-
-        ArrayDeque<Integer> queueStack = new ArrayDeque<>();
-        for (int i = 0; i < N; i++) {
-            if (A[i] == 0) { // 큐일 때만 값 관리
-                queueStack.add(B[i]);
+    public static int solution(String str, String searchChar) {
+        int cnt = 0;
+        char c = searchChar.charAt(0);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                cnt++;
             }
         }
+        return cnt;
+    }
 
-        for (int i = 0; i < M; i++) {
-            queueStack.addFirst(C[i]);
-            stringBuilder.append(queueStack.pollLast()).append(" ");
-        }
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-        System.out.println(stringBuilder);
+        String str = bufferedReader.readLine().toLowerCase();
+        String searchChar = bufferedReader.readLine().toLowerCase();
+        System.out.println(solution(str, searchChar));
     }
 
 }
