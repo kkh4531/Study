dbe104ef63ad7e35340e1f5123ae3091c4cc9fc2
kimkihyeon <rlgus4531@naver.com>
2025-02-13 17:02:59 +0900

    인프런 자바 알고리즘 인강 1-02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,43 +5,52 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-1. 문자 찾기
+2. 대소문자 변환
 설명
 
-한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
 
-대소문자를 구분하지 않습니다.문자열의 길이는 100을 넘지 않습니다.
 
 입력
 
-첫 줄에 문자열이 주어지고, 두 번째 줄에 문자가 주어진다.
+첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
 
 문자열은 영어 알파벳으로만 구성되어 있습니다.
 
 
 출력
 
-첫 줄에 해당 문자의 개수를 출력한다
+첫 줄에 대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 출력합니다.
+
+
+예시 입력 1
+
+StuDY
+예시 출력 1
+
+sTUdy
  */
 public class Main {
 
-    public static int solution(String str, String searchChar) {
-        int cnt = 0;
-        char c = searchChar.charAt(0);
+    public static StringBuilder solution(String str) {
+        StringBuilder res = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == c) {
-                cnt++;
+            char c = str.charAt(i);
+            if (c >= 'a' && c <= 'z') { // 소문자면
+                c = (char) (c - 32);
+                res.append(c);
+            } else {
+                res.append((char) (c + 32));
             }
         }
-        return cnt;
+        return res;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = bufferedReader.readLine().toLowerCase();
-        String searchChar = bufferedReader.readLine().toLowerCase();
-        System.out.println(solution(str, searchChar));
+        String str = bufferedReader.readLine();
+        System.out.println(solution(str));
     }
 
 }
