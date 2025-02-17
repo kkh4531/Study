093a9644495bda1fcb638cb1459704484bd7c150
kimkihyeon <rlgus4531@naver.com>
2025-02-17 22:53:54 +0900

    자바 인프런 알고리즘 강의 1-04 StringBuilder reverse 메소드 이용

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -34,32 +34,20 @@ g0en2T0s8eSoft
  */
 public class Main {
 
-    public static String solution(String str) {
-        String answer = "";
-        int max = Integer.MIN_VALUE;
-        int pos;
-        while((pos = str.indexOf(' ')) != -1) { // 공백을 발견하는 인덱스 위치(pos) 즉 공백을 발견한 인덱스 위치가 -1이 아니면 true
-            String temp = str.substring(0, pos);
-            int size = temp.length();
-            if (size > max) {
-                max = size;
-                answer = temp;
-            }
-            str = str.substring(pos + 1); // 원본 str에서 pos는 공백이니까 pos +1부터해서 문자열을 자른다.
-        } // 여기까지 한다면 마지막 단어는 검사를 못한다 왜냐면 study 뒤에는 공백이 없기 때문에
-        // 그러면 마지막 단어도 검사해야한다면
-        if (str.length() > max) { // 현재 str은 맨 마지막 단어를 가지고 있다.
-            answer = str;
+    public static StringBuilder solution(int N, BufferedReader bufferedReader) throws Exception {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String temp = new StringBuilder(bufferedReader.readLine()).reverse().toString();
+            res.append(temp).append('\n');
         }
-        return answer;
+        return res;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = bufferedReader.readLine();
-
-        System.out.println(solution(str));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        System.out.println(solution(N, bufferedReader));
     }
 
 }
