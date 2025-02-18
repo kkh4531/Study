3a304a83a2816ea053c50d3daf93758c62df262d
kimkihyeon <rlgus4531@naver.com>
2025-02-18 19:50:59 +0900

    자바 인프런 알고리즘 강의 1-11

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,63 +5,73 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-10. 가장 짧은 문자거리
+11. 문자열 압축
 설명
 
-한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
+알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는
+
+문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오.
+
+단 반복횟수가 1인 경우 생략합니다.
 
 
 입력
 
-첫 번째 줄에 문자열 s와 문자 t가 주어진다. 문자열과 문자는 소문자로만 주어집니다.
-
-문자열의 길이는 100을 넘지 않는다.
+첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
 
 
 출력
 
-첫 번째 줄에 각 문자열 s의 각 문자가 문자 t와 떨어진 거리를 순서대로 출력한다.
+첫 줄에 압축된 문자열을 출력한다.
 
 
 예시 입력 1
 
-teachermode e
+KKHSSSSSSSE
 예시 출력 1
 
-1 0 1 2 1 0 1 2 2 1 0
+K2HS7E
+예시 입력 2
+
+KSTTTSEEKFKKKDJJGG
+예시 출력 2
+
+KST3SE2KFK3DJ2G2
  */
 public class Main {
-    public static StringBuilder solution(String str1, String str2) {
-        ArrayList<Integer> indexes = new ArrayList<>();
+    public static StringBuilder solution(String str) {
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) == str2.charAt(0)) {
-                indexes.add(i); // 문자 t의 인덱스 위치를 가지고 있는 리스트 정의 완료
+        char old = str.charAt(0);
+        int cnt = 1;
+        for (int i = 1; i < str.length(); i++) {
+            char c = str.charAt(i); // 현재 문자
+            if (old == c) { // 전 문자와 현재 문자가 같은 경우
+                cnt++;
+            } else { // 전 문자와 현재 문자가 다를 경우
+                if (cnt == 1) {
+                    res.append(old);
+                } else {
+                    res.append(old).append(cnt);
+                }
+                cnt = 1;
             }
-        }
-
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) == str2.charAt(0)) { // 문자열 s의 인덱스 i가 문자 t와 같다면 그 자체니깐 0
-                res.append(0).append(" ");
-                continue;
-            }
-            int min = Integer.MAX_VALUE;
-            for (int j = 0; j < indexes.size(); j++) { // 현재 문자열 s의 i번째인 문자와 문자 t의 인덱스들과 비교 후 최솟값 찾는다.
-                int temp = Math.abs(i - indexes.get(j));
-                if (temp < min) {
-                    min = temp;
+            old = c;
+            if (i == str.length() - 1) { // 마지막 인덱스
+                // 마지막 인덱스가 하나인 경우
+                if (cnt == 1) {
+                    res.append(old);
+                } else { // 마지막 인덱스가 계속 이어지는 경우
+                    res.append(old).append(cnt);
                 }
             }
-            res.append(min).append(" ");
         }
-        res.deleteCharAt(res.length() - 1);
         return res;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str[] = bufferedReader.readLine().split(" ");
-        System.out.println(solution(str[0], str[1]));
+        String str = bufferedReader.readLine();
+        System.out.println(solution(str));
     }
 
 }
