279a30c2d4a0021dc22df135f20a5f8df5c51658
kimkihyeon <rlgus4531@naver.com>
2025-02-20 15:06:23 +0900

    자바 인프런 알고리즘 강의 1-12

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,64 +4,37 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/*
-11. 문자열 압축
-설명
-
-알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는
-
-문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오.
-
-단 반복횟수가 1인 경우 생략합니다.
-
-
-입력
-
-첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
-
-
-출력
-
-첫 줄에 압축된 문자열을 출력한다.
-
-
-예시 입력 1
-
-KKHSSSSSSSE
-예시 출력 1
-
-K2HS7E
-예시 입력 2
-
-KSTTTSEEKFKKKDJJGG
-예시 출력 2
-
-KST3SE2KFK3DJ2G2
- */
 public class Main {
-    public static StringBuilder solution(String str) {
-        StringBuilder res = new StringBuilder();
-        str += " ";
-        int cnt = 1;
-        for (int i = 0; i < str.length() - 1; i++) { // 현재 i랑 i+1일 비교할 때 빈 문자열을 str에 더하면 마지막 인덱스도 확인할 수 있다.
-            if (str.charAt(i) == str.charAt(i + 1)) { // 같은 문자일 때는
-                cnt++;
-            } else { // 문자가 달라졌을 때는
-                if (cnt == 1) {
-                    res.append(str.charAt(i));
+
+    public static String solution(String str[]) {
+        String temp[] = new String[str.length];
+        String res = "";
+        for (int i = 0; i < str.length; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (str[i].charAt(j) == '#') {
+                    temp[i] += "1";
                 } else {
-                    res.append(str.charAt(i)).append(cnt);
+                    temp[i] += "0";
                 }
-                cnt = 1;
             }
+            temp[i] = temp[i].substring(4);
+            int decimal = Integer.parseInt(temp[i], 2);
+            res += (char) decimal;
         }
+
         return res;
     }
-
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine());
         String str = bufferedReader.readLine();
-        System.out.println(solution(str));
+        String res[] = new String[N];
+        for (int i = 0; i < N; i++) {
+            res[i] = str.substring(0, 7);
+            str = str.substring(7);
+        }
+        System.out.println(solution(res));
     }
 
 }
