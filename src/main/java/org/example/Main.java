c4ab0b5ffd835f58d389a0485cde8ff45d8eb251
kimkihyeon <rlgus4531@naver.com>
2025-02-21 23:25:13 +0900

    자바 인프런 알고리즘 강의 2-02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -32,60 +32,30 @@ import java.util.*;
 5
  */
 public class Main {
-    // 1 가위 2 바위 3 보
-    // 1이 이기는 경우 3 질 경우 2
-    // 2가 이기는 경우 1 질 경우 3
-    // 3이 이기는 경우 2 질 경우 1
-    public static StringBuilder solution(int N, int A[], int B[]) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            int a = A[i];
-            int b = B[i];
-            if (a == 1) {
-                if (b == 1) {
-                    sb.append("D");
-                } else if (b == 2) {
-                    sb.append("B");
-                } else {
-                    sb.append("A");
-                }
-            } else if (a == 2) {
-                if (b == 1) {
-                    sb.append("A");
-                } else if (b == 2) {
-                    sb.append("D");
-                } else {
-                    sb.append("B");
-                }
-            } else { // a가 3일 때
-                if (b == 1) {
-                    sb.append("B");
-                } else if (b == 2) {
-                    sb.append("A");
-                } else {
-                    sb.append("D");
-                }
+
+    public static int solution(int arr[]) {
+        int res = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                res++;
             }
-            sb.append('\n');
         }
-        return sb;
+        return res;
     }
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine());
-        int A[] = new int[N];
-        int B[] = new int[N];
-        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        StringTokenizer st2 = new StringTokenizer(bufferedReader.readLine());
 
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-            B[i] = Integer.parseInt(st2.nextToken());
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        int arr[] = new int[st.countTokens()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(solution(N, A, B));
+        System.out.println(solution(arr));
     }
-
 
 }
