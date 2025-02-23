911f4a3ebe6488ddfee9bf1ec4a529523e9fb5c1
kimkihyeon <rlgus4531@naver.com>
2025-02-23 20:31:16 +0900

    자바 인프런 알고리즘 강의 2-05 에라토스테네스의 체

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,58 +5,58 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-4. 피보나치 수열
+5. 소수(에라토스테네스 체)
 설명
 
-1) 피보나키 수열을 출력한다. 피보나치 수열이란 앞의 2개의 수를 합하여 다음 숫자가 되는 수열이다.
+자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
 
-2) 입력은 피보나치 수열의 총 항의 수 이다. 만약 7이 입력되면 1 1 2 3 5 8 13을 출력하면 된다.
+만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.
 
 
 입력
 
-첫 줄에 총 항수 N(3<=N<=45)이 입력된다.
+첫 줄에 자연수의 개수 N(2<=N<=200,000)이 주어집니다.
 
 
 출력
 
-첫 줄에 피보나치 수열을 출력합니다.
+첫 줄에 소수의 개수를 출력합니다.
 
 
 예시 입력 1
 
-10
+20
 예시 출력 1
 
-1 1 2 3 5 8 13 21 34 55
+8
  */
 public class Main {
+//에라토스테네스의 체
+    //소수가 되는 수를 찾고 그 수의 배수들을 싹 다 없앤다.
 
-    public static int arr[];
-
+    //true이면 소수x false이면 소수
     public static int solution(int N) {
-        if (N == 0) {
-            return 0;
-        } else if (N == 1) {
-            return 1;
-        } else if (arr[N] != 0) { // 첫 번째 0은 이미 맨 처음 if에서 걸러짐
-            return arr[N];
+        int res = 0;
+        for (int i = 2; i <= N; i++) {
+            if (!arr[i]) { // false이면 즉 아직 소수인지 검증이 안된 얘면 or 소수이면
+                for (int j = i + i; j <= N; j += i) {
+                    arr[j] = true;
+                }
+                res++;
+            }
         }
-        arr[N] = solution(N - 2) + solution(N - 1);
-        return arr[N];
+        return res;
     }
+
+    public static boolean arr[];
+
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine());
 
-        arr = new int[N+1];
-        arr[0] = 0;
-        arr[1] = 1;
-        solution(N);
-        for (int i = 1; i < N + 1; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println(solution(N));
+
     }
 
 }
