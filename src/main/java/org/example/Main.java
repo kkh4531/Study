3ebe3fdf37a096e9dc22ed22855628b1e0ccedc9
kimkihyeon <rlgus4531@naver.com>
2025-02-23 17:11:12 +0900

    자바 인프런 알고리즘 강의 2-04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,57 +5,58 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-2. 보이는 학생
+4. 피보나치 수열
 설명
 
-선생님이 N명의 학생을 일렬로 세웠습니다. 일렬로 서 있는 학생의 키가 앞에서부터 순서대로 주어질 때, 맨 앞에 서 있는
+1) 피보나키 수열을 출력한다. 피보나치 수열이란 앞의 2개의 수를 합하여 다음 숫자가 되는 수열이다.
 
-선생님이 볼 수 있는 학생의 수를 구하는 프로그램을 작성하세요. (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
+2) 입력은 피보나치 수열의 총 항의 수 이다. 만약 7이 입력되면 1 1 2 3 5 8 13을 출력하면 된다.
 
 
 입력
 
-첫 줄에 정수 N(5<=N<=100,000)이 입력된다. 그 다음줄에 N명의 학생의 키가 앞에서부터 순서대로 주어진다.
+첫 줄에 총 항수 N(3<=N<=45)이 입력된다.
 
 
 출력
 
-선생님이 볼 수 있는 최대학생수를 출력한다.
+첫 줄에 피보나치 수열을 출력합니다.
 
 
 예시 입력 1
 
-8
-130 135 148 140 145 150 150 153
+10
 예시 출력 1
 
-5
+1 1 2 3 5 8 13 21 34 55
  */
 public class Main {
 
-    public static int solution(int arr[]) {
-        int res = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                res++;
-            }
+    public static int arr[];
+
+    public static int solution(int N) {
+        if (N == 0) {
+            return 0;
+        } else if (N == 1) {
+            return 1;
+        } else if (arr[N] != 0) { // 첫 번째 0은 이미 맨 처음 if에서 걸러짐
+            return arr[N];
         }
-        return res;
+        arr[N] = solution(N - 2) + solution(N - 1);
+        return arr[N];
     }
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine());
 
-        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        int arr[] = new int[st.countTokens()];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        arr = new int[N+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        solution(N);
+        for (int i = 1; i < N + 1; i++) {
+            System.out.print(arr[i] + " ");
         }
-
-        System.out.println(solution(arr));
     }
 
 }
