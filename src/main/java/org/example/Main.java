8f9daaefa017edef09735987fcd794af95f574ea
kimkihyeon <rlgus4531@naver.com>
2025-03-04 20:33:09 +0900

    자바 인프런 알고리즘 강의 2-10 dx,dy를 이용한 시계방향 인덱스 탐색 풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,96 +5,79 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-11. 임시반장 정하기
+10. 봉우리
 설명
 
-김갑동 선생님은 올해 6학년 1반 담임을 맡게 되었다.
+지도 정보가 N*N 격자판에 주어집니다. 각 격자에는 그 지역의 높이가 쓰여있습니다.
 
-김갑동 선생님은 우선 임시로 반장을 정하고 학생들이 서로 친숙해진 후에 정식으로 선거를 통해 반장을 선출하려고 한다.
+각 격자판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 봉우리 지역이 몇 개 있는 지 알아내는 프로그램을 작성하세요.
 
-그는 자기반 학생 중에서 1학년부터 5학년까지 지내오면서 한번이라도 같은 반이었던 사람이 가장 많은 학생을 임시 반장으로 정하려 한다.
+격자의 가장자리는 0으로 초기화 되었다고 가정한다.
 
-그래서 김갑동 선생님은 각 학생들이 1학년부터 5학년까지 몇 반에 속했었는지를 나타내는 표를 만들었다.
-
-예를 들어 학생 수가 5명일 때의 표를 살펴보자.
+만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
 
 Image1.jpg
-
-위 경우에 4번 학생을 보면 3번 학생과 2학년 때 같은 반이었고, 3번 학생 및 5번 학생과 3학년 때 같은 반이었으며,
-
-2번 학생과는 4학년 때 같은 반이었음을 알 수 있다. 그러므로 이 학급에서 4번 학생과 한번이라도
-
-같은 반이었던 사람은 2번 학생, 3번 학생과 5번 학생으로 모두 3명이다.
-
-이 예에서 4번 학생이 전체 학생 중에서 같은 반이었던 학생 수가 제일 많으므로 임시 반장이 된다.
-
-각 학생들이 1학년부터 5학년까지 속했던 반이 주어질 때, 임시 반장을 정하는 프로그램을 작성하시오.
 
 
 입력
 
-첫째 줄에는 반의 학생 수를 나타내는 정수가 주어진다. 학생 수는 3 이상 1000 이하이다.
+첫 줄에 자연수 N이 주어진다.(2<=N<=50)
 
-둘째 줄부터는 1번 학생부터 차례대로 각 줄마다 1학년부터 5학년까지 몇 반에 속했었는지를 나타내는 5개의 정수가 빈칸 하나를 사이에 두고 주어진다.
-
-주어지는 정수는 모두 1 이상 9 이하의 정수이다.
+두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는다.
 
 
 출력
 
-첫 줄에 임시 반장으로 정해진 학생의 번호를 출력한다.
-
-단, 임시 반장이 될 수 있는 학생이 여러 명인 경우에는 그 중 가장 작은 번호만 출력한다.
+봉우리의 개수를 출력하세요.
 
 
 예시 입력 1
 
 5
-2 3 1 7 3
-4 1 9 6 8
-5 5 2 4 4
-6 5 2 6 7
-8 4 2 2 2
+5 3 7 2 3
+3 7 1 6 1
+7 2 5 3 4
+4 3 6 4 1
+8 7 3 5 2
 예시 출력 1
 
-4
+10
  */
 public class Main {
 
+    public static int dx[] = {-1, 0, 1, 0};
+    public static int dy[] = {0, 1, 0, -1};
+
     public static int solution(int n, int arr[][]) {
         int res = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) { // i번 학생
-            int cnt = 0;
-            boolean boolArr[] = new boolean[n]; // 전부 false로 돼있음 즉 아직 겹치지 않은 학생은 false상태 한 번이라도 겹친 학생은 true
-            for (int j = 0; j < 5; j++) { // 5학년까지
-                int presentStudent = arr[i][j];
-                for (int k = 0; k < n; k++) { // 본인 제외 반이 겹쳤는지 모든 학생 탐색
-                    if ((i != k) && !boolArr[k]) { // 본인은 제외, true 즉 이미 한번 겹쳤던 이력이 있는 학생이지 패스하고 false인데 반이 겹친다? 해당 학생 true로 바꾸고 cnt++
-                        if (presentStudent == arr[k][j]) {
-                            cnt++;
-                            boolArr[k] = true;
-                        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k]; // 현재 인덱스의 12시부터 시작. 시계 방향으로 k=1일 때는 3시방향 k=2일 때는 6시 방향... 행
+                    int ny = j + dy[k]; // 열
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] > arr[i][j]) { // ArrayIndexOutOfBoundsException을 피하기 위해선 nx, ny가 -1이나 n 이상이면 안됨, 시계 방향으로 돌면서 현재 인덱스보다 값이 큰 게 있으면 봉우리가 아니므로 false
+                        flag = false;
+                        break;
                     }
                 }
-            }
-            if (cnt > max) {
-                max = cnt;
-                res = i;
+                if (flag) {
+                    res++;
+                }
             }
         }
-        return res + 1;
+        return res;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine());
-        int arr[][] = new int[N][5];
+        int arr[][] = new int[N][N];
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(bufferedReader.readLine());
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
