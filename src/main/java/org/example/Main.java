e5241dcd242af088853f697105c49e3a06d05ee1
kimkihyeon <rlgus4531@naver.com>
2025-03-05 21:55:07 +0900

    자바 인프런 알고리즘 강의 3-02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,60 +5,59 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-1. 두 배열 합치기
+2. 공통원소 구하기
 설명
 
-오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
+A, B 두 개의 집합이 주어지면 두 집합의 공통 원소를 추출하여 오름차순으로 출력하는 프로그램을 작성하세요.
 
 
 입력
 
-첫 번째 줄에 첫 번째 배열의 크기 N(1<=N<=100)이 주어집니다.
+첫 번째 줄에 집합 A의 크기 N(1<=N<=30,000)이 주어집니다.
 
-두 번째 줄에 N개의 배열 원소가 오름차순으로 주어집니다.
+두 번째 줄에 N개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다.
 
-세 번째 줄에 두 번째 배열의 크기 M(1<=M<=100)이 주어집니다.
+세 번째 줄에 집합 B의 크기 M(1<=M<=30,000)이 주어집니다.
 
-네 번째 줄에 M개의 배열 원소가 오름차순으로 주어집니다.
+네 번째 줄에 M개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다.
 
-각 리스트의 원소는 int형 변수의 크기를 넘지 않습니다.
+각 집합의 원소는 1,000,000,000이하의 자연수입니다.
 
 
 출력
 
-오름차순으로 정렬된 배열을 출력합니다.
+두 집합의 공통원소를 오름차순 정렬하여 출력합니다.
 
 
 예시 입력 1
 
-3
-1 3 5
 5
-2 3 6 7 9
+1 3 9 5 2
+5
+3 2 5 7 8
 예시 출력 1
 
-1 2 3 3 5 6 7 9
+2 3 5
  */
 public class Main {
 
-    public static StringBuilder solution(int n, int m, int a[], int b[]) {
+    public static StringBuilder solution(int n, int m, StringTokenizer st, StringTokenizer st2) {
+        HashSet<Integer> set = new HashSet<>();
         StringBuilder sb = new StringBuilder();
-        int pa = 0, pb = 0;
-        while (pa < n && pb < m) {
-            if (a[pa] > b[pb]) { // a[pa]가 클 경우에만 출력
-                sb.append(b[pb++]).append(" ");
-            } else { // b[pb]가 클 때 출력 + a[pa]랑 b[pb]가 같은 경우도 b[pb]를 출력하고 ++
-                sb.append(a[pa++]).append(" ");
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            int temp = Integer.parseInt(st2.nextToken());
+            if (!set.add(temp)) {
+                list.add(temp);
             }
         }
-        //이제 어느 한 쪽은 배열의 끝까지 도달했다. 그러면 나머지 남은 배열을 쭈루룩 그대로 갖다 붙인다.
-        while (pa < n) { // 배열 b가 먼저 다 끝났다면 a 배열 나머지들을 출력할 것이다.
-            sb.append(a[pa++]).append(" ");
+        Collections.sort(list);
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append(" ");
         }
-        while (pb < m) { // 배열 a가 먼저 다 끝났다면 b 배열 나머지들을 출력할 것이다.
-            sb.append(b[pb++]).append(" ");
-        }
-        // a,b 중 누가 먼저 끝났는 지 알 수 없으니 둘 다 반복문 실행한 것. 끝난 쪽은 반복문이 실행되지 않겠지?
         return sb;
     }
 
@@ -66,18 +65,11 @@ public class Main {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine());
-        int a[] = new int[N];
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < N; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
-        }
         int M = Integer.parseInt(bufferedReader.readLine());
-        int b[] = new int[M];
-        st = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0 ; i < M; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
-        }
-        System.out.println(solution(N, M, a, b));
+        StringTokenizer st2 = new StringTokenizer(bufferedReader.readLine());
+
+        System.out.println(solution(N, M, st, st2));
     }
 
 }
