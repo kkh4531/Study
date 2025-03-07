68b71111d70f309afdaf1ba409efb5a84613fe5b
kimkihyeon <rlgus4531@naver.com>
2025-03-07 20:41:30 +0900

    자바 인프런 알고리즘 강의 3-04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,40 +5,79 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
+4. 연속 부분수열
+설명
+
+N개의 수로 이루어진 수열이 주어집니다.
+
+이 수열에서 연속부분수열의 합이 특정숫자 M이 되는 경우가 몇 번 있는지 구하는 프로그램을 작성하세요.
+
+만약 N=8, M=6이고 수열이 다음과 같다면
+
+1 2 1 3 1 1 1 2
+
+합이 6이 되는 연속부분수열은 {2, 1, 3}, {1, 3, 1, 1}, {3, 1, 1, 1}로 총 3가지입니다.
+
+
+입력
+
+첫째 줄에 N(1≤N≤100,000), M(1≤M≤100,000,000)이 주어진다.
+
+수열의 원소값은 1,000을 넘지 않는 자연수이다.
+
+
+출력
+
+첫째 줄에 경우의 수를 출력한다.
+
+
+예시 입력 1
+
+8 6
+1 2 1 3 1 1 1 2
+예시 출력 1
+
+3
  */
 public class Main {
-// 2중 반복문을 쓰면 시간 초과가 뜨게 된다.
-    // sum에서 i-k 번째 인덱스를 빼고 현재 i를 더해주면서 나아가면 1중 반복문으로도 풀 수 있다.
-    public static int solution(int n, int k, int arr[]) {
-        int max = 0;
+    public static int solution(int n, int m, int arr[]) {
         int sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum += arr[i];
-        }
-        max = sum;
-        for (int i = k; i < n; i++) {
-            sum = sum - arr[i - k] + arr[i];
-            if (sum > max) {
-                max = sum;
+        int cnt = 0;
+
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            if (sum == m) {
+                cnt++;
+                sum -= arr[temp];
+                temp += 1;
+                sum += arr[i];
+            } else if (sum > m) {
+                sum -= arr[temp];
+                i -= 1;
+                temp += 1;
+            } else {
+                sum += arr[i];
             }
         }
-        return max;
+
+        if (sum == m) { // 마지막에 더해진 애는 검사가 안 이루어져서 검사해야함.
+            cnt++;
+        }
+        return cnt;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        int arr[] = new int[N];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int arr[] = new int[n];
         st = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        System.out.println(solution(N, K, arr));
+        System.out.println(solution(n, m, arr));
     }
 
 }
