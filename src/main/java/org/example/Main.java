d443fcf4a179e2797a2a640243a17f50eaf7ebdc
kimkihyeon <rlgus4531@naver.com>
2025-03-14 16:06:04 +0900

    자바 인프런 알고리즘 강의 4-02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,29 +5,25 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    public static char solution(int n, String str) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        char value = '\u0000';
-        for (int i = 0; i < n; i++) {
-            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
-        }
-        int max = Integer.MIN_VALUE;
-        for (char c : map.keySet()) {
-            if (map.get(c) > max) {
-                max = map.get(c);
-                value = c;
+    public static String solution(char c1[], char c2[]) {
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        for (int i = 0; i < c1.length; i++) {
+            if (c1[i] != c2[i]) {
+                return "NO";
             }
         }
-        return value;
+        return "YES";
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(bufferedReader.readLine());
-        String str = bufferedReader.readLine();
-        System.out.println(solution(n, str));
+        String str1 = bufferedReader.readLine();
+        char c1[] = str1.toCharArray();
+        String str2 = bufferedReader.readLine();
+        char c2[] = str2.toCharArray();
+        System.out.println(solution(c1, c2));
     }
 
 }
