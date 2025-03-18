d403ae55fa9a78157b823faa4ca4658e88b993e9
kimkihyeon <rlgus4531@naver.com>
2025-03-18 21:46:08 +0900

    자바 인프런 알고리즘 강의 4-03 정답 풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,27 +8,22 @@ public class Main {
 
     public static StringBuilder solution(int n, int k, int arr[]) {
         StringBuilder sb = new StringBuilder();
-        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < k; i++) {
-            set.add(arr[i]);
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        sb.append(set.size()).append(" ");
+        sb.append(map.size()).append(" ");
 
         int lt = 0;
         for (int i = k; i < n; i++) {
-            boolean flag = true;
-            for (int j = lt + 1; j < i; j++) { // lt+1부터 i 전까지 즉 지금 삭제하려는 arr[lt] 이후의 윈도우 안에 같은 값이 있는지 확인하는 반복문
-                if (arr[lt] == arr[j]) {
-                    flag = false; // flag가 false이면 지금 삭제하려는 arr[lt] 값이 또 있는 것
-                }
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            if (map.get(arr[lt]) == 0) {
+                map.remove(arr[lt]);
             }
-            if (flag) { // arr[lt]를 제외한 윈도우 안에 arr[lt]와 같은 값이 없음.
-                set.remove(arr[lt]);
-            }
-            set.add(arr[i]);
-            sb.append(set.size()).append(" ");
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
             lt++;
+            sb.append(map.size()).append(" ");
         }
         sb.deleteCharAt(sb.length() - 1);
         return sb;
