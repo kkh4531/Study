fee4442e799eb69b86d1a7b79f52e083d1d828e3
kimkihyeon <rlgus4531@naver.com>
2025-03-18 21:25:13 +0900

    자바 인프런 알고리즘 강의 4-03 time limit 떴으나 값은 조건에 맞게 나온 코드

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,49 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static String solution(char c1[], char c2[]) {
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        for (int i = 0; i < c1.length; i++) {
-            if (c1[i] != c2[i]) {
-                return "NO";
-            }
+
+    public static StringBuilder solution(int n, int k, int arr[]) {
+        StringBuilder sb = new StringBuilder();
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < k; i++) {
+            set.add(arr[i]);
         }
-        return "YES";
+        sb.append(set.size()).append(" ");
+
+        int lt = 0;
+        for (int i = k; i < n; i++) {
+            boolean flag = true;
+            for (int j = lt + 1; j < i; j++) { // lt+1부터 i 전까지 즉 지금 삭제하려는 arr[lt] 이후의 윈도우 안에 같은 값이 있는지 확인하는 반복문
+                if (arr[lt] == arr[j]) {
+                    flag = false; // flag가 false이면 지금 삭제하려는 arr[lt] 값이 또 있는 것
+                }
+            }
+            if (flag) { // arr[lt]를 제외한 윈도우 안에 arr[lt]와 같은 값이 없음.
+                set.remove(arr[lt]);
+            }
+            set.add(arr[i]);
+            sb.append(set.size()).append(" ");
+            lt++;
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String str1 = bufferedReader.readLine();
-        char c1[] = str1.toCharArray();
-        String str2 = bufferedReader.readLine();
-        char c2[] = str2.toCharArray();
-        System.out.println(solution(c1, c2));
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(bufferedReader.readLine());
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(n, k, arr));
     }
 
 }
