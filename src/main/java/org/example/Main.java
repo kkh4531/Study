4b1d2151cb4843b2c7ffd4388ec4eb90af4a8518
kimkihyeon <rlgus4531@naver.com>
2025-03-25 19:31:19 +0900

    자바 인프런 알고리즘 강의 5-04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,51 +5,39 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    public static int solution(int n, int arr[][], int k, int moves[]) {
+    public static int solution(String str) {
         int result = 0;
         Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = 0; i < k; i++) {
-            int crane = moves[i]; // 열
-            for (int j = 0; j < n; j++) { // 행
-                if (arr[j][crane - 1] != 0) {
-                    if (!dq.isEmpty()) { // stack이 비어있지 않다면
-                        if (dq.getLast() == arr[j][crane - 1]) { // stack의 마지막 숫자가 넣으려는 숫자와 같다면
-                            dq.removeLast();
-                            result += 2;
-                        } else {
-                            dq.addLast(arr[j][crane - 1]);
-                        }
-                    } else {
-                        dq.addLast(arr[j][crane - 1]);
-                    }
-                    arr[j][crane - 1] = 0;
-                    break;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isDigit(c)) { // 숫자이면
+                dq.addLast(Character.getNumericValue(c));
+            } else { // 연산자이면
+                int second = dq.pollLast();
+                int first = dq.pollLast();
+                int tempResult = 0;
+                switch (c) {
+                    case '*' :
+                        tempResult = first * second; break;
+                    case '+' :
+                        tempResult = first + second; break;
+                    case '-' :
+                        tempResult = first - second; break;
+                    case '/' :
+                        tempResult = first / second; break;
                 }
+                dq.addLast(tempResult);
             }
         }
+        result = dq.poll();
         return result;
     }
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(bufferedReader.readLine());
-        int arr[][] = new int[n][n];
-        StringTokenizer st;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(bufferedReader.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int k = Integer.parseInt(bufferedReader.readLine());
-        int moves[] = new int[k];
-        st = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < k; i++) {
-            moves[i] = Integer.parseInt(st.nextToken());
-        }
+        String str = bufferedReader.readLine();
 
-        System.out.println(solution(n, arr, k, moves));
+        System.out.println(solution(str));
     }
 
 }
