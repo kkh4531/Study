f62a92f8dfcd35da5e9f35895e17308d69481e47
kimkihyeon <rlgus4531@naver.com>
2025-03-25 16:49:16 +0900

    자바 인프런 알고리즘 강의 5-03

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,30 +2,54 @@ package org.example;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    public static StringBuilder solution(String input) {
-        StringBuilder result = new StringBuilder();
-        Deque<String> dq = new ArrayDeque<>();
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '(') {
-                dq.add("(");
-            } else if (input.charAt(i) == ')') {
-                dq.removeLast();
-            } else if (dq.isEmpty()) {
-                result.append(input.charAt(i));
+
+    public static int solution(int n, int arr[][], int k, int moves[]) {
+        int result = 0;
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < k; i++) {
+            int crane = moves[i]; // 열
+            for (int j = 0; j < n; j++) { // 행
+                if (arr[j][crane - 1] != 0) {
+                    if (!dq.isEmpty()) { // stack이 비어있지 않다면
+                        if (dq.getLast() == arr[j][crane - 1]) { // stack의 마지막 숫자가 넣으려는 숫자와 같다면
+                            dq.removeLast();
+                            result += 2;
+                        } else {
+                            dq.addLast(arr[j][crane - 1]);
+                        }
+                    } else {
+                        dq.addLast(arr[j][crane - 1]);
+                    }
+                    arr[j][crane - 1] = 0;
+                    break;
+                }
             }
         }
         return result;
     }
-
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = bufferedReader.readLine();
-        System.out.println(solution(input));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int arr[][] = new int[n][n];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int k = Integer.parseInt(bufferedReader.readLine());
+        int moves[] = new int[k];
+        st = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < k; i++) {
+            moves[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(n, arr, k, moves));
     }
 
 }
