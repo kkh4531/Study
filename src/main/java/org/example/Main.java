47e459d425100f48a13f668937cf525192d2e78e
kimkihyeon <rlgus4531@naver.com>
2025-03-28 00:37:45 +0900

    자바 인프런 알고리즘 강의 5-08

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,28 +5,50 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static String solution(String target, String lectures) {
-        Queue<Character> queue = new ArrayDeque<>();
-        for (int i = 0; i < target.length(); i++) {
-            queue.add(target.charAt(i));
-        }
-        for (int i = 0; i < lectures.length(); i++) {
-            if (lectures.charAt(i) == queue.peek()) {
-                queue.poll();
+    public static int solution(int n, int m, LinkedList<int[]> list) {
+        int result = 0;
+        int cnt = 1;
+        while (true) {
+            int max = list.peek()[0];
+            int maxPos = 0;
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i)[0] > max) {
+                    max = list.get(i)[0];
+                    maxPos = i;
+                }
             }
-            if (queue.isEmpty()) {
-                return "YES";
+            for (int i = 0; i < maxPos; i++) {
+                list.add(list.poll());
             }
+            int first[] = list.poll();
+            if (first[1] == 1) {
+                result = cnt;
+                break;
+            }
+            cnt++;
         }
-        return "NO";
+        return result;
     }
+
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String target = bufferedReader.readLine();
-        String lectures = bufferedReader.readLine();
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        System.out.println(solution(target, lectures));
+        st = new StringTokenizer(bufferedReader.readLine());
+        LinkedList<int[]> list = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (i == m) {
+                int temp[] = {Integer.parseInt(st.nextToken()), 1};
+                list.add(temp);
+            } else {
+                int temp[] = {Integer.parseInt(st.nextToken()), 0};
+                list.add(temp);
+            }
+        }
+        System.out.println(solution(n, m, list));
     }
 
 }
