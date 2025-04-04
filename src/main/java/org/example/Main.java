8fdc20880a60a849e553fbd349cfdf757025ba0a
kimkihyeon <rlgus4531@naver.com>
2025-04-04 19:19:00 +0900

    자바 인프런 알고리즘 강의 6-05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,37 +5,22 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static StringBuilder solution(int n, int arr[]) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < n; i++) {
-            int tempIdx = i;
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr[tempIdx] < arr[j]) {
-                    int temp = arr[tempIdx];
-                    arr[tempIdx] = arr[j];
-                    arr[j] = temp;
-                    tempIdx--;
-                }
-            }
-        }
+    public static String solution(int n, StringTokenizer st) {
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
-            sb.append(arr[i]).append(" ");
+            int tmp = Integer.parseInt(st.nextToken());
+            if (set.contains(tmp)) {
+                return "D";
+            }
+            set.add(tmp);
         }
-        return sb;
+        return "U";
     }
-
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bufferedReader.readLine());
-
-        int arr[] = new int[n];
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        System.out.println(solution(n, arr));
+        System.out.println(solution(n, st));
     }
-
 }
