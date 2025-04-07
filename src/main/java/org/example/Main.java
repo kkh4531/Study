8e65e3318279751daf399c76dc8c69a32366833e
kimkihyeon <rlgus4531@naver.com>
2025-04-07 18:37:55 +0900

    자바 인프런 알고리즘 강의 6-06

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,14 @@ import java.util.*;
 
 public class Main {
 
-    public static StringBuilder solution(int n, List<Location> list) {
-        Comparator<Location> comparator = new Comparator<Location>() {
-            @Override
-            public int compare(Location o1, Location o2) {
-                if (o1.getX() == o2.getX()) {
-                    return o1.getY() - o2.getY();
-                }
-                return o1.getX() - o2.getX();
-            }
-        };
-        Collections.sort(list, comparator);
+    public static StringBuilder solution(int n, int arr[]) {
+        int tmp[] = arr.clone();
+        Arrays.sort(tmp);
         StringBuilder sb = new StringBuilder();
-        for (Location l : list) {
-            sb.append(l.getX()).append(" ").append(l.getY()).append('\n');
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != tmp[i]) {
+                sb.append(i + 1).append(" ");
+            }
         }
         return sb;
     }
@@ -29,32 +23,12 @@ public class Main {
 
         int n = Integer.parseInt(bufferedReader.readLine());
 
-        List<Location> list = new ArrayList<>();
-        StringTokenizer st;
+        int arr[] = new int[n];
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(bufferedReader.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list.add(new Location(x, y));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(solution(n, list));
-    }
-}
-
-class Location {
-    private int x;
-    private int y;
-    public Location(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY() {
-        return this.y;
+        System.out.println(solution(n, arr));
     }
 }
