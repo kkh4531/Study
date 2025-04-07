2885714d4fc1369bd60ac1cb746dd8bdd1bdac83
kimkihyeon <rlgus4531@naver.com>
2025-04-07 18:37:20 +0900

    자바 인프런 알고리즘 강의 6-07

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,45 +5,56 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static StringBuilder solution(int s, int n, int arr[]) {
-        StringBuilder sb = new StringBuilder();
-        LinkedList<Integer> queue = new LinkedList<>();
 
-        for (int i = 0; i < n; i++) {
-            int tmp = arr[i];
-            if (!queue.contains(tmp)) { // 큐에 현재 작업하려는 숫자가 없을 경우 = 캐시 미스
-                if (queue.size() >= s) { // 사이즈가 다 찼을 경우
-                    queue.poll();
-                    queue.offer(tmp);
-                } else {
-                    queue.offer(tmp);
+    public static StringBuilder solution(int n, List<Location> list) {
+        Comparator<Location> comparator = new Comparator<Location>() {
+            @Override
+            public int compare(Location o1, Location o2) {
+                if (o1.getX() == o2.getX()) {
+                    return o1.getY() - o2.getY();
                 }
-            } else { // 캐시 히츠
-                int idx = 0;
-                for (int j = 0; j < queue.size(); j++) {
-                    if (queue.get(j) == tmp) {
-                        queue.offer(queue.remove(idx));
-                        break;
-                    }
-                    idx++;
-                }
+                return o1.getX() - o2.getX();
             }
-        }
-        for (int i = queue.size() - 1; i >= 0; i--) {
-            sb.append(queue.get(i)).append(" ");
+        };
+        Collections.sort(list, comparator);
+        StringBuilder sb = new StringBuilder();
+        for (Location l : list) {
+            sb.append(l.getX()).append(" ").append(l.getY()).append('\n');
         }
         return sb;
     }
+
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        int s = Integer.parseInt(st.nextToken()); // 캐시 사이즈
-        int n = Integer.parseInt(st.nextToken()); // 작업 갯수
-        int arr[] = new int[n];
-        st = new StringTokenizer(bufferedReader.readLine());
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        List<Location> list = new ArrayList<>();
+        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(bufferedReader.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new Location(x, y));
         }
-        System.out.println(solution(s, n, arr));
+
+        System.out.println(solution(n, list));
+    }
+}
+
+class Location {
+    private int x;
+    private int y;
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
     }
 }
