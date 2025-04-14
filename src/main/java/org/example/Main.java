f2215ed966635a90a8a5c5d04e85266889ed32b5
kimkihyeon <rlgus4531@naver.com>
2025-04-14 21:22:52 +0900

    자바 인프런 알고리즘 강의 6-10 결정알고리즘(매개변수 탐색)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,50 +7,49 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int m;
+    static int c;
     static int arr[];
+
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(bufferedReader.readLine());
         arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
         System.out.println(solution());
     }
+
     public static int solution() {
         int result = 0;
-        int lt = arr[n - 1];
-        int rt = 0;
-        for (int i = 0; i < n; i++) {
-            rt += arr[i];
-        }
+        int lt = 1;
+        int rt = arr[n - 1] - 1;
         while (lt <= rt) {
             int mid = (lt + rt) / 2;
-            if (func(mid)) { // m 이하이면
+            if (func(mid)) { // true = cnt >= c;
                 result = mid;
-                rt = mid - 1;
-            } else { // m 초과이면
                 lt = mid + 1;
+            } else {
+                rt = mid - 1;
             }
         }
         return result;
     }
-    public static boolean func(int t) {
-        int sum = 0;
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            sum += arr[i];
-            if (sum > t) {
-                sum = arr[i];
+
+    public static boolean func(int d) {
+        int cnt = 1;
+        int ep = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - ep >= d) {
                 cnt++;
+                ep = arr[i];
             }
         }
-        cnt++;
-        return cnt <= m;
+        return cnt >= c;
     }
 }
