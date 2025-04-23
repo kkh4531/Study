46725950a7fadb4b83a32bee83f9d414e88973be
kimkihyeon <rlgus4531@naver.com>
2025-04-23 21:49:20 +0900

    자바 인프런 알고리즘 강의 경로탐색(DFS)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,75 +4,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Node {
-    int data;
-    Node lt, rt;
-
-    public Node(int data) {
-        this.data = data;
-    }
-}
-
 public class Main {
+    static int n;
+    static boolean visit[]; // 이미 지나온 정점으로 다시 돌아가면 안되니 체크 배열
+    static boolean graph[][]; // 정점에서 정점으로 방향이 있는 그래프
+    static int cnt;
 
-    public static void bfs(Node root) {
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
-                Node current = q.poll();
-                if (current.lt != null) {
-                    q.add(current.lt);
-                }
-                if (current.rt != null) {
-                    q.add(current.rt);
-                }
+    public static void dfs(int v) {
+        if (v == n) {
+            cnt++;
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!visit[i] && graph[v][i]) {
+                visit[i] = true;
+                dfs(i);
+                visit[i] = false;
             }
         }
     }
-
-    static List<Integer> check = new ArrayList<>();
-
-    public static int solution(int s, int e) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(s);
-        int L = 0;
-        boolean flag = true;
-        check.add(s);
-        while (!q.isEmpty()) {
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
-                if (q.peek() == e) {
-                    flag = false;
-                    break;
-                } else {
-                    int tmp = q.poll();
-                    if (!check.contains(tmp + 1)) {
-                        check.add(tmp + 1);
-                        q.offer(tmp + 1);
-                    }
-                    if (!check.contains(tmp - 1)) {
-                        check.add(tmp - 1);
-                        q.offer(tmp - 1);
-                    }
-                    if (!check.contains(tmp + 5)) {
-                        check.add(tmp + 5);
-                        q.offer(tmp + 5);
-                    }
-                }
-            }
-            if (!flag) {
-                break;
-            }
-            L++;
-        }
-        return L;
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        System.out.println(solution(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        visit = new boolean[n + 1];
+        graph = new boolean[n + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(bufferedReader.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            graph[from][to] = true;
+        }
+        visit[1] = true; // 정점 1부터 시작하니 1은 이미 방문한 상태
+        dfs(1);
+        System.out.println(cnt);
     }
 }
