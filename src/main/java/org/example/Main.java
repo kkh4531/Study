0df314d48f5ac0fe710ad6974c038577fc1e4eea
kimkihyeon <rlgus4531@naver.com>
2025-04-24 19:56:52 +0900

    자바 인프런 알고리즘 강의 08-02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,52 +5,36 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    static boolean visit[];
-    static ArrayList<ArrayList<Integer>> graph;
     static int n;
+    static int input[];
+    static int sum = 0;
+    static int c;
+    static int max = Integer.MIN_VALUE;
 
-    public static StringBuilder bfs(int v) {
-        StringBuilder sb = new StringBuilder();
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(v);
-        visit[v] = true;
-        int L = 0;
-        while (!queue.isEmpty()) {
-            sb.append(L).append(" : ");
-            int len = queue.size();
-            for (int i = 0; i < len; i++) {
-                int cv = queue.poll();
-                sb.append(cv).append(" ");
-                for (int nv : graph.get(cv)) {
-                    if (!visit[nv]) {
-                        visit[nv] = true;
-                        queue.offer(nv);
-                    }
-                }
+    public static void dfs(int depth) {
+        if (depth == n) {
+            if (sum <= c) {
+                max = Math.max(sum, max);
             }
-            L++;
-            sb.append('\n');
+            return;
         }
-        return sb;
+        sum += input[depth];
+        dfs(depth + 1);
+        sum -= input[depth];
+        dfs(depth + 1);
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        c = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        visit = new boolean[n + 1];
-        graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+        input = new int[n];
+        for (int i = 0; i < n; i++) {
+            int tmp = Integer.parseInt(bufferedReader.readLine());
+            input[i] = tmp;
         }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(bufferedReader.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            graph.get(from).add(to);
-        }
-        System.out.println(bfs(1));
+        dfs(0);
+        System.out.println(max);
     }
 }
