2ffbe48a9b1ea15e9d6c09f189e1572715f7e2cd
kimkihyeon <rlgus4531@naver.com>
2025-04-24 16:53:49 +0900

    자바 인프런 알고리즘 강의 최단경로(BFS) 인접리스트

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,42 +5,52 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static boolean visit[]; // 이미 지나온 정점으로 다시 돌아가면 안되니 체크 배열
-    static int cnt;
-    static ArrayList<ArrayList<Integer>> graph;
 
-    public static void dfs(int v) {
-        if (v == n) {
-            cnt++;
-            return;
-        }
-        for (int nv : graph.get(v)) {
-            if (!visit[nv]) { // 아직 방문하지 않은 정점이면
-                visit[nv] = true;
-                dfs(nv);
-                visit[nv] = false;
+    static boolean visit[];
+    static ArrayList<ArrayList<Integer>> graph;
+    static int n;
+
+    public static StringBuilder bfs(int v) {
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+        visit[v] = true;
+        int L = 0;
+        while (!queue.isEmpty()) {
+            sb.append(L).append(" : ");
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                int cv = queue.poll();
+                sb.append(cv).append(" ");
+                for (int nv : graph.get(cv)) {
+                    if (!visit[nv]) {
+                        visit[nv] = true;
+                        queue.offer(nv);
+                    }
+                }
             }
+            L++;
+            sb.append('\n');
         }
+        return sb;
     }
+
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
         n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        visit = new boolean[n + 1];
         graph = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(bufferedReader.readLine());
-            int from = Integer.parseInt(st.nextToken()); // 정점
-            int to = Integer.parseInt(st.nextToken()); // 정점
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
             graph.get(from).add(to);
         }
-        visit = new boolean[n + 1];
-        visit[1] = true;
-        dfs(1);
-        System.out.println(cnt);
+        System.out.println(bfs(1));
     }
 }
