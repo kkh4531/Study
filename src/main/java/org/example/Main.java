2fe3520da7bd77b470cb83bdbafea4012d781042
kimkihyeon <rlgus4531@naver.com>
2025-04-28 23:53:22 +0900

    자바 인프런 알고리즘 강의 08-07

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,44 +6,53 @@ import java.util.*;
 
 public class Main {
 
-    static int arr[];
-    static boolean visit[] = new boolean[501];
-    public static int bfs(int n, int t) {
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            q.offer(arr[i]);
-            visit[arr[i]] = true;
-        }
-        int L = 1;
-        while (!q.isEmpty()) {
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
-                int cv = q.poll();
-                if (cv == t) {
-                    return L;
-                } else {
-                    for (int j = 0; j < arr.length; j++) {
-                        if (!visit[cv + arr[j]]) {
-                            q.offer(cv + arr[j]);
-                            visit[cv + arr[j]] = true;
-                        }
-                    }
-                }
+//    static int arr[];
+//    static boolean visit[] = new boolean[501];
+//    public static int bfs(int n, int t) {
+//        Queue<Integer> q = new LinkedList<>();
+//        for (int i = 0; i < n; i++) {
+//            q.offer(arr[i]);
+//            visit[arr[i]] = true;
+//        }
+//        int L = 1;
+//        while (!q.isEmpty()) {
+//            int len = q.size();
+//            for (int i = 0; i < len; i++) {
+//                int cv = q.poll();
+//                if (cv == t) {
+//                    return L;
+//                } else {
+//                    for (int j = 0; j < arr.length; j++) {
+//                        if (!visit[cv + arr[j]]) {
+//                            q.offer(cv + arr[j]);
+//                            visit[cv + arr[j]] = true;
+//                        }
+//                    }
+//                }
+//            }
+//            L++;
+//        }
+//        return 0;
+//    }
+    static int arr[][];
+    public static int dfs(int n, int r) {
+        if (r == 0 || n == r) {
+            return 1;
+        } else {
+            if (arr[n][r] != 0) {
+                return arr[n][r];
+            } else {
+                return arr[n][r] = dfs(n - 1, r - 1) + dfs(n - 1, r);
             }
-            L++;
         }
-        return 0;
     }
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(bufferedReader.readLine());
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-        arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        int t = Integer.parseInt(bufferedReader.readLine());
-        System.out.println(bfs(n, t));
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1][r + 1];
+        System.out.println(dfs(n, r));
     }
 }
