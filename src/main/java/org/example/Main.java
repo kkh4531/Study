f5ce04b1807a337c70b46d4fc073fb24a44a5ee8
kimkihyeon <rlgus4531@naver.com>
2025-05-13 18:55:47 +0900

    자바 인프런 알고리즘 강의 08-13 dfs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,52 +13,44 @@ public class Main {
     static int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
     static int cnt = 0;
 
-    public static void bfs() {
-        Queue<int[]> queue = new LinkedList<>();
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(bufferedReader.readLine());
+        board = new int[n + 1][n + 1];
+        visit = new boolean[n + 1][n + 1];
+
+        StringTokenizer st;
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 1; j <= n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (!visit[i][j] && board[i][j] == 1) {
-                    queue.offer(new int[]{i, j});
+                if (board[i][j] == 1 && !visit[i][j]) {
                     visit[i][j] = true;
-                    while (!queue.isEmpty()) {
-                        int tmp[] = queue.poll();
-                        int cy = tmp[0];
-                        int cx = tmp[1];
-                        for (int k = 0; k < 8; k++) {
-                            int ny = cy + dy[k];
-                            int nx = cx + dx[k];
-                            if (ny >= 1 && ny <= n && nx >= 1 && nx <= n && board[ny][nx] == 1 && !visit[ny][nx]) {
-                                visit[ny][nx] = true;
-                                queue.offer(new int[]{ny, nx});
-                            }
-                        }
-                    }
+                    dfs(i, j);
                     cnt++;
                     i = 1;
                     j = 1;
                 }
             }
         }
+
+        System.out.println(cnt);
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(bufferedReader.readLine());
-        visit = new boolean[n + 1][n + 1];
-        board = new int[n + 1][n + 1];
-
-        StringTokenizer st;
-        for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(bufferedReader.readLine());
-            for (int j = 1; j <= n; j++) {
-                int tmp = Integer.parseInt(st.nextToken());
-                if (tmp == 1) {
-                    board[i][j] = 1;
-                }
+    public static void dfs(int cy, int cx) {
+        for (int i = 0; i < 8; i++) {
+            int ny = cy + dy[i];
+            int nx = cx + dx[i];
+            if (nx >= 1 && nx <= n && ny >= 1 && ny <= n && board[ny][nx] == 1 && !visit[ny][nx]) {
+                visit[ny][nx] = true;
+                dfs(ny, nx);
             }
         }
-        bfs();
-        System.out.println(cnt);
     }
 }
