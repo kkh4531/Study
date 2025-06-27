b8029f207765b83eeacd6dd5327a5fd04b2a15a8
kimkihyeon <rlgus4531@naver.com>
2025-06-27 20:05:30 +0900

    자바 인프런 알고리즘 강의 09 - 02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,57 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int dfs(int n) {
-        if (n == 1) return 1;
-        return n * dfs(n - 1);
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print(dfs(5));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        ArrayList<Time> list = new ArrayList<>();
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bufferedReader.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            list.add(new Time(start, end));
+        }
+
+        System.out.print(solution(list));
+    }
+
+    public static int solution(ArrayList<Time> times) {
+        Collections.sort(times);
+        int cnt = 0;
+        int ep = 0;
+        for (Time t : times) {
+            if (t.getStartTime() >= ep) {
+                ep = t.getEndTime();
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+
+class Time implements Comparable<Time> {
+    private int startTime;
+    private int endTime;
+
+    Time(int startTime, int endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public int getStartTime() {
+        return this.startTime;
+    }
+
+    public int getEndTime() {
+        return this.endTime;
+    }
+
+    @Override
+    public int compareTo(Time o) {
+        if (this.endTime == o.endTime) return this.startTime - o.startTime;
+        return this.endTime - o.endTime;
     }
 }
