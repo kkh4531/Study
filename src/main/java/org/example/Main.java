08ce55373380cfaeac09ba24bab9665bcc71036e
kimkihyeon <rlgus4531@naver.com>
2025-07-22 15:36:38 +0900

    자바 인프런 알고리즘 강의 10 - 01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,56 +6,27 @@ import java.util.*;
 
 public class Main {
 
+    static int cnt = 0;
+
+    public static void dfs(int sum, int n) {
+        int[] arr = {1, 2};
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum == n) {
+                cnt++;
+                sum -= arr[i];
+            } else {
+                if (sum < n) dfs(sum, n);
+                sum -= arr[i];
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bufferedReader.readLine());
-        ArrayList<Time> list = new ArrayList<>();
-        StringTokenizer st;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(bufferedReader.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            list.add(new Time(start, end));
-        }
-
-        System.out.print(solution(list));
-    }
-
-    public static int solution(ArrayList<Time> times) {
-        Collections.sort(times);
-        int cnt = 0;
-        int ep = 0;
-        for (Time t : times) {
-            if (t.getStartTime() >= ep) {
-                ep = t.getEndTime();
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-}
-
-class Time implements Comparable<Time> {
-    private int startTime;
-    private int endTime;
-
-    Time(int startTime, int endTime) {
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-    public int getStartTime() {
-        return this.startTime;
-    }
-
-    public int getEndTime() {
-        return this.endTime;
-    }
-
-    @Override
-    public int compareTo(Time o) {
-        if (this.endTime == o.endTime) return this.startTime - o.startTime;
-        return this.endTime - o.endTime;
+        dfs(0, n);
+        System.out.println(cnt);
     }
 }
